aa5ad49c01117b19c6d2e409cd37380b86a82aa5
Valter Santos <valter.c.santos@protonmail.com>
2025-02-03 19:41:38 +1300

    Progress

--- src/client/main.ts
+++ src/client/main.ts
@@ -19,6 +19,11 @@ async function processPdfFile(file: File) {
   // In this initial state, each entry will have the "unverified" state
   const citations = await ParseCitations(file);
 
+  if (typeof citations === "string") {
+    console.log(citations);
+    return;
+  }
+
   console.log("Return is..!");
   console.log(citations);
   for (let i = 0; i < citations.length; i++) {
@@ -28,11 +33,10 @@ async function processPdfFile(file: File) {
   // Afterwards, we check if each citation actually exists.
 }
 
-// Generate 30 sample items
 function addCitationToDom(citation: CitationInformation, id: number) {
   const li = document.createElement("li");
   li.className =
-    "p-4 hover:bg-gray-50 transition-colors cursor-pointer flex items-center";
+    "p-4 hover:bg-gray-50 transition-colors cursor-pointer flex items-center mt-1 mb-1 border-2 border-gray-300 rounded-lg shadow-md ";
 
   // Create number badge
   const badge = document.createElement("span");
@@ -41,24 +45,48 @@ function addCitationToDom(citation: CitationInformation, id: number) {
   badge.textContent = `${id}`;
 
   // Create text content
-  const paperName = document.createElement("span");
-  paperName.className = "text-gray-700";
-  paperName.textContent = citation.paper;
 
   const authors = document.createElement("span");
-  authors.className = "text-gray-700";
+  authors.className = "text-gray-700 text-left mb-3";
   authors.textContent = citation.authors;
+
+  const paperName = document.createElement("span");
+  paperName.className = "text-gray-700 text-left mb-3";
+  paperName.textContent = citation.paper;
+
+  const conference = document.createElement("span");
+  conference.className = "text-gray-700 text-left mb-3";
+  conference.textContent = citation.conference;
+
+  // Initial state - Loading
+  const stateDiv = document.createElement("div");
+  stateDiv.className = "flex items-center space-x-2";
+  stateDiv.id = `citation-state-${id}`;
+
+  const stateWheel = document.createElement("span");
+  stateWheel.className =
+    "inline-block w-4 h-4 border-3 border-t-transparent border-blue-500 rounded-full animate-spin";
+  const stateText = document.createElement("span");
+  stateText.className = "text-gray-700 text-left";
+  stateText.textContent = "Querying...";
+  stateDiv.appendChild(stateText);
+  stateDiv.appendChild(stateWheel);
 
   li.appendChild(badge);
 
   const originalText = document.createElement("span");
-  originalText.className = "text-gray-700 truncate";
+  originalText.className = "text-gray-500 truncate mb-3 text-left";
   originalText.textContent = citation.originalText;
+  originalText.title = citation.originalText;
 
   const paperInformation = document.createElement("div");
-  paperInformation.className = "flex flex-col";
+  paperInformation.className = "flex flex-col max-w-9/10";
   paperInformation.appendChild(authors);
   paperInformation.appendChild(paperName);
+  paperInformation.appendChild(conference);
+  paperInformation.appendChild(originalText);
+  paperInformation.appendChild(stateDiv);
+
   li.appendChild(paperInformation);
   itemsList.appendChild(li);
 }
