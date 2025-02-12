9fe9c6410c4fb5f6651df43c012e13c546b00385
Valter Santos <valter.c.santos@protonmail.com>
2025-02-12 20:09:42 +1300

    Improve tokenizer to handle more complex scenarios

--- client/src/logic/tokenizer.ts
+++ client/src/logic/tokenizer.ts
@@ -30,15 +30,16 @@ export class CitationTokenizer {
   }
 
   // Same as consumeUntil, but does not consume
-  private peekUntil(predicate: (consumedText: string, char: string) => boolean): string {
+  private peekUntil(
+    predicate: (consumedText: string, char: string) => boolean,
+    start?: number,
+  ): string {
     let result = "";
-    let startingPosition = this.position;
 
-    while (
-      startingPosition < this.input.length &&
-      !predicate(result, this.peek(startingPosition))
-    ) {
-      result += this.input[startingPosition++];
+    let pos = start != undefined ? start : this.position;
+
+    while (pos < this.input.length && !predicate(result, this.peek(pos))) {
+      result += this.input[pos++];
     }
     return result;
   }
@@ -63,26 +64,48 @@ export class CitationTokenizer {
   //   - Conover, Mary Boudreau. Understanding electrocardiography.
   //   - Einstein, Albert. "The general theory of relativity."
   //   - Valente, Andreia, et al. "Empathic aurea:
+  //   - Anisi, David A. Optimal motion control
+  //   - Sel, Alejandra, Ruben T. Azevedo, and Manos Tsakiris. "Heartfelt self:
+  //   - Anisi, David A., Erik Persson, and Clint Heyer. "Real-world demonstration
+  //   - T. Azevedo, Ruben, et al. "The calming effect of a new
+  //
   //
   // APA:
   //   - Conover, M. B. (2002).
   //   - Einstein, A. (1922). The general theory of relativity.
   //   - Valente, A., Lopes, D. S., Nunes, N., & Esteves, A. (2022, March).
+  //   - Anisi, D. A. (2003). Optimal motion
+  //   - Sel, A., Azevedo, R. T., & Tsakiris, M. (2017). Heartfelt self:
+  //   - Anisi, D. A., Persson, E., & Heyer, C. (2011, September). Real-world demonstration
+  //   - T. Azevedo, R., Bennett, N., Bilicki, A., Hooper, J., Markopoulou, F., & Tsakiris, M. (2017). The calming effect
   //
   // CHICAGO:
   //   - Conover, Mary Boudreau. Understanding electrocardiography.
   //   - Einstein, Albert. "The general theory of relativity."
   //   - Valente, Andreia, Daniel Simoes Lopes, Nuno Nunes, and Augusto Esteves. "Empathic aurea:
+  //   - Anisi, David A. "Optimal motion control
+  //   - Sel, Alejandra, Ruben T. Azevedo, and Manos Tsakiris. "Heartfelt self:
+  //   - Anisi, David A., Erik Persson, and Clint Heyer. "Real-world demonstration
+  //   - T. Azevedo, Ruben, Nell Bennett, Andreas Bilicki, Jack Hooper, Fotini Markopoulou, and Manos Tsakiris. "The calming
+  //
   //
   // HARVARD:
   //   - Conover, M.B., 2002. Understanding electrocardiography
   //   - Einstein, A., 1922. The general theory of relativity.
   //   - Valente, A., Lopes, D.S., Nunes, N. and Esteves, A., 2022, March. Empathic aurea:
+  //   - Anisi, D.A., 2003. Optimal motion control
+  //   - Sel, A., Azevedo, R.T. and Tsakiris, M., 2017. Heartfelt self: cardio-visual
+  //   - Anisi, D.A., Persson, E. and Heyer, C., 2011, September. Real-world demonstration
+  //   - T. Azevedo, R., Bennett, N., Bilicki, A., Hooper, J., Markopoulou, F. and Tsakiris, M., 2017. The calming effect
   //
   // VANCOUVER:
   //   - Conover MB. Understanding electrocardiography.
   //   - Einstein A. The general theory of relativity.
   //   - Valente A, Lopes DS, Nunes N, Esteves A. Empathic aurea:
+  //   - Anisi DA. Optimal motion control
+  //   - Sel A, Azevedo RT, Tsakiris M. Heartfelt self: cardio-visual
+  //   - Anisi DA, Persson E, Heyer C. Real-world demonstration
+  //   - T. Azevedo R, Bennett N, Bilicki A, Hooper J, Markopoulou F, Tsakiris M. The calming effect
   //
   // UNKNOWN:
   //   - M. B. Conover.
@@ -90,36 +113,36 @@ export class CitationTokenizer {
   //   - L. C. Li.
   //
   private parseAuthors(): string {
+    // First, we get all the text until the first special character.
     let text = this.peekUntil((_, char) => char === "," || char === "." || char === ":");
 
+    //  --- UNKNOWN STLYE IDENTIFICATION ---
     // If there is only one character and then a full stop, then it's the unknown format.
     if (text.length === 1) {
+      console.log("unknown");
       return this.consumeUntil(() => this.isEndOfAuthorsUnknownFormat());
     }
 
-    // Vancouver style doesn't have a comma after the first name, so it is the easiest to identify.
-    // It ends when we find the first full stop.
+    // --- VANCOUVER IDENTIFICATION ---
+    // Vancouver style is always last name (full) and then only the initials of the author.
+    // It always contains an year, so we can look for it.
     if (text.includes(" ")) {
-      return this.consumeUntil((_, char) => char === "." || char === ":");
+      return this.consumeUntil(() => this.isEndOfAuthorsByFindingYear());
+      //return this.consumeUntil((_, char) => char === "." || char === ":");
     }
 
-    // Next, check if the second name isn't an initial.
-    // Parse until two commas are found, or a full stop is found
-    // If it isn't, then it's MLA or CHICAGO, while both can be parsed until a final stop is found
-    text = this.peekUntil(
-      (peekedText, char) =>
-        (peekedText.includes(",") && char === ",") || char === "." || char === ":",
-    );
-
-    const splitText = text.split(",");
-    const textHasTwoElements = splitText.length === 2;
-    if (textHasTwoElements && splitText[1].length === 1) {
-      return this.consumeUntil((_, char) => char === "." || char === ":");
+    // --- APA and HARVARD ---
+    // Both of these styles start with a full word (more than 2 characters before the first comma) and then only have initials, followed by a year.
+    if (this.authorsTextIsApaOrHarvard()) {
+      console.log("apa");
+      this.consumeUntil(() => this.isEndOfAuthorsByFindingYear());
     }
 
-    // APA and HARVARD are missing by this point
-    // Both end with the year after the authors section, so we try to parse it
-    return this.consumeUntil(() => this.isEndOfAuthorsByFindingYear());
+    // --- MLA AND CHICAGO IDENTIFICATION ---
+    // MLA always ends with a final stop and white space, without a comma in the next word.
+    // Since these are the last formats, parse the data expecting this assumption to be true.
+    console.log("mla");
+    return this.parseAuthorsInMlaOrChicagoFormat();
   }
 
   private isEndOfAuthorsUnknownFormat(): boolean {
@@ -143,6 +166,70 @@ export class CitationTokenizer {
     );
   }
 
+  // First we collect all text until the first comma,
+  // which we verify that is longer than 2 characters (so not an initial).
+  // Then, we expect to find an initial.
+  // If find the initial, then it means that this is Apa or Harvard.
+  private authorsTextIsApaOrHarvard(): boolean {
+    var pos = this.position;
+    var text = "";
+    // Step 1: Ensure the first word is not an initial
+    while (pos < this.input.length) {
+      const currentChar = this.input[pos++];
+      text += currentChar;
+
+      if (currentChar === ",") {
+        if (text.length === 1) {
+          return false;
+        } else {
+          break;
+        }
+      }
+    }
+
+    text = "";
+    // Step 2: Ensure the next thing we find is an initial.
+    while (pos < this.input.length) {
+      const currentChar = this.input[pos++];
+
+      // Skip whitespace
+      if (currentChar != " ") {
+        text += currentChar;
+      }
+
+      // Found the full stop. If we have an initial by this point, then it is APA/Harvard!
+      if (currentChar === ".") {
+        return text.length === 1;
+      }
+    }
+
+    return false;
+  }
+
+  // The first author is always separated with two commas.
+  // If there is only a comma then a full stop without any commas, then it is a single author.
+  // Otherwise, we end when we find a full stop after a word (not initial)
+  private parseAuthorsInMlaOrChicagoFormat(): string {
+    var text = "";
+    text += this.consumeUntil((_, c) => c === ",");
+    text += this.consumeUntil((_, c) => c === ".");
+
+    // If there is only one author, then we return it.
+    if (this.input[this.position + 1] != ",") {
+      return text;
+    }
+
+    text += this.consumeUntil((consumedText, c) => {
+      if (c != ".") {
+        return false;
+      }
+
+      const lastChar = consumedText[consumedText.length - 1];
+      return lastChar.toUpperCase() !== lastChar;
+    });
+
+    return text;
+  }
   // If we find a number, then we assume that is year the year and return true.
   private isEndOfAuthorsByFindingYear(): boolean {
     let pos = this.position;
